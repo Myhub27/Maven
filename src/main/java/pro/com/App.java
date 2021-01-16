99153e079ce670d2f5345b1c574bc89ebacb6eb9
root <root@ip-172-31-33-83.ap-south-1.compute.internal>
2021-01-16 12:22:17 +0000

    commit

---
+++ src/main/java/pro/com/App.java
@@ -0,0 +1,13 @@
+package pro.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
